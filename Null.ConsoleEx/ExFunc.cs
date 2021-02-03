265c7306c2eac6b2fe9d400301c3f190ffd9db07
SlimeNull <slimenull@outlook.com>
2021-02-04 01:44:15 +0800

    添加了ConsoleExS类

--- Null.ConsoleEx/ExFunc.cs
+++ Null.ConsoleEx/ExFunc.cs
@@ -29,4 +29,33 @@ namespace Null.ConsoleEx
             return total;
         }
     }
+    public static class ConsoleExS
+    {
+        static byte[] Lengths = Resources.Lengths;
+        public static bool IsWideDisplayChar(char c)
+        {
+            int index = c;
+            return (Lengths[index / 8] & (1 << (index % 8))) != 0;
+        }
+        public static int GetCharDisplayLength(char c)
+        {
+            int index = c;
+            bool wideChar = (Lengths[index / 8] & (1 << (index % 8))) != 0;
+
+            return wideChar ? 2 : 1;
+        }
+        public static int GetStringDisplayLength(string str)
+        {
+            if (str == null)
+                throw new ArgumentNullException("str");
+
+            int total = 0;
+            foreach (char c in str)
+            {
+                int i = c;
+                total += (Lengths[i / 8] & (1 << (i % 8))) != 0 ? 2 : 1;
+            }
+            return total;
+        }
+    }
 }
