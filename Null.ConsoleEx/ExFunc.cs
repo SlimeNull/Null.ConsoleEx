bee2b0604b448676a94e76230bcb916ae1056b7b
SlimeNull <slimenull@outlook.com>
2021-02-04 01:31:39 +0800

    添加项目文件。

---
+++ Null.ConsoleEx/ExFunc.cs
@@ -0,0 +1,32 @@
+﻿using Null.ConsoleEx.Properties;
+using System;
+
+namespace Null.ConsoleEx
+{
+    public static class ConsoleEx
+    {
+        static byte[] Lengths = Resources.Lengths;
+        public static bool IsWideDisplayChar(char c)
+        {
+            int index = c;
+            return (Lengths[index / 8] & (1 << (index % 8))) != 0;
+        }
+        public static int GetCharDisplayLength(char c)
+        {
+            int index = c;
+            bool wideChar = (Lengths[index / 8] & (1 << (index % 8))) != 0;
+
+            return wideChar ? 2 : 1;
+        }
+        public static int GetStringDisplayLength(string str)
+        {
+            int total = 0;
+            foreach (char c in str)
+            {
+                int i = c;
+                total += (Lengths[i / 8] & (1 << (i % 8))) != 0 ? 2 : 1;
+            }
+            return total;
+        }
+    }
+}
